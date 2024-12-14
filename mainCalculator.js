636b8a337ce62ca2f818312372ca8b5b76ae45c9
Testaccount335 <119867843+Testaccount335@users.noreply.github.com>
2024-12-14 16:57:03 -0500

    Update mainCalculator.js

--- mainCalculator.js
+++ mainCalculator.js
@@ -3,18 +3,18 @@ var toggleSlope = true
 var _getElement = document.getElementById
 submit.addEventListener("click", function()
 {
-  let y1Value = _getElement("y1").value;
-  let y2Value = _getElement("y2").value;
-  let x1Value = _getElement("x1").value;
-  let x2Value = _getElement("x2").value;
+  let y1Value = getElement("y1").value;
+  let y2Value = getElement("y2").value;
+  let x1Value = getElement("x1").value;
+  let x2Value = getElement("x2").value;
 
   let yValue = y1Value - y2Value
   let xValue = x1Value - x2Value
   var slopeAnswer = yValue/xValue
   var slopeFractionAnswer = `${yValue}/${xValue}`
   
-  _getElement("slopeAnswer").innerHTML = `The Slope Is: ${slopeAnswer}`
-  _getElement("slopeFractionAnswer").innerHTML = `The fraction is ${slopeFractionAnswer}`
+  getElement("slopeAnswer").innerHTML = `The Slope Is: ${slopeAnswer}`
+  getElement("slopeFractionAnswer").innerHTML = `The fraction is ${slopeFractionAnswer}`
   console.log(`The Slope Is: ${slopeAnswer}`)
   if (toggleSlope == true){
     
